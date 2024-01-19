a48aae6c1d9acf3c59d13cbb6ae77dd096da8ab3
fmg75 <f.mg@outlook.com>
2024-01-18 21:41:53 -0300

    id

--- Alert_bot.py
+++ Alert_bot.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import time
 from telegram import Bot
+
 from lxml import html
 #import config
 
@@ -11,19 +12,30 @@ url = 'https://www.coingecko.com/es/monedas/universal-basic-income'
 telegram_token = st.secrets["TOKEN"]
 
 def scrape_valor(url):
+    while True:
+        try:
             # Realizar la solicitud HTTP, simula un navegador....
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
             }
             response = requests.get(url, headers=headers)
+
             tree = html.fromstring(response.content)
             # Utilizar XPath para encontrar el elemento span específico
             xpath = '/html/body/div[3]/main/div[1]/div[1]/div/div[1]/div[2]/div/div[1]/span[1]/span'
             valor_element = tree.xpath(xpath)
+
+            # Verificar si valor_element está vacío
+            if not valor_element:
+                raise ValueError("No se pudo encontrar el elemento span específico")
+
             # Extraer el contenido del elemento en formato adecuado
             valor = valor_element[0].text_content().strip()
             return float(valor.replace('$', '').replace(',', '.'))
-        
+        except Exception as e:
+            print(f"Error al obtener el valor: {e}")
+            time.sleep(1)
+
 # Función para enviar alerta a Telegram
 def enviar_alerta_telegram(token, chat_id, mensaje):
     bot = Bot(token)
@@ -35,13 +47,13 @@ def interface():
     st.markdown("[Iniciar conversación con el bot de Telegram](https://t.me/Alert_7011371_bot)")
     
     response = requests.get(f'https://api.telegram.org/bot{telegram_token}/getUpdates')
-    chat_id = response.json()['result'][0]['message']['chat']['id']
 
+# Obtener el chat_id desde la respuesta
+    chat_id = response.json()['result'][0]['message']['chat']['id']
     # Inicializar valor_objetivo fuera de la condición
     valor_objetivo = st.session_state.get('valor_objetivo', 0.00100000)
     valor_actual = scrape_valor(url)
     st.write('Valor actual UBI', valor_actual)
-
     # Verificar si se ingresó un chat_id y mostrar el campo Alerta cuando supere
     if chat_id:
         input_key = "valor_objetivo_input"
